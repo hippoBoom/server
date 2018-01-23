dc908914d21e6cc00e9629ea9f6338d2a6f66356
commpanyPc <youdetian@foxmail.com>
2018-01-23 17:30:20 +0800

    done

--- routes/faqs.js
+++ routes/faqs.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Faqs = require('../models/faqs');
 
-mongoose.connect('mongodb://127.0.0.1:36911/cdb')
+mongoose.connect('mongodb://47.104.156.124:36911/cdb')
+
 mongoose.connection.on('connected', () => {
     console.log("MongoDB connected success.")
 })
